d925323e8153534d986204df68951f6a1f3d2ac7
Nguyễn Thái Sang <146358825+sangneko@users.noreply.github.com>
2026-03-08 14:58:14 +0700

    Delete src/app directory

--- src/app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import './globals.css'
-export const metadata = { title: 'Sang Music Cloud', description: 'Personal Music Library' }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (<html><body>{children}</body></html>)
-}
